db7b439223cc14774005f8f4e0a67eee233d104d
d-martirosyan <david.martirosyan.2112@gmail.com>
2025-06-09 22:02:58 +0400

    Add labarator 2

---
+++ labarator-2/airport-model.pl
@@ -0,0 +1,11 @@
+% Օդանավակայան-Թռիչք հարաբերություն
+airport_flights(airport, flight).
+
+% Ուղևոր-Ավիատոմս հարաբերություն
+passenger_ticket(passenger, ticket).
+
+% Թռիչք-Ժամանակ հարաբերություն
+flight_time(flight, time).
+
+% Ավիատոմսի տվյալներ
+ticket_details(ticket, flight_number, direction, time).
